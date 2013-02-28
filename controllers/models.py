0eea8174e669fc1a61f5825af7ebbd4bf0902daf
Guillermo Bergengruen <guillermo.bergengruen@ingesur.com.uy>
2013-02-28 13:12:11 -0200

    actualizado

--- controllers/models.py
+++ controllers/models.py
@@ -31,16 +31,19 @@ def mosivo():
                     DIV(
                         DIV(DIV(INPUT(_id='rectCB', _name='rectCB', _type='checkbox'), _class='igsCB'), DIV(LABEL("Rectángulo:"), _class="igsOtherLabel")),
                         DIV(LABEL("Esquina superior izquierda:"), _class='igsTabbedLabel'), 
-                        DIV(LABEL("Latitud:"), INPUT(_type='text', _id ="topLeftLat", _name ="topLeftLat", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"), LABEL("Longitud:"), INPUT(_type='text', _id ="topLeftLong", _name ="topLeftLong", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"),_class="igsNumbers"), 
+                        DIV(LABEL("Latitud:"), INPUT(_type='text', _id ="topLeftLat", _name ="topLeftLat", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"), LABEL("Longitud:"), INPUT(_type='text', _id ="topLeftLong", _name ="topLeftLong", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"),_class="igsNumbers"),
+                        DIV(_style="clear: both;"),
                         DIV(LABEL("Esquina inferior derecha:"), _class='igsTabbedLabel'),
                         DIV(LABEL("Latitud:"), INPUT(_type='text', _id ="bottomRightLat", _name ="bottomRightLat", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"), LABEL("Longitud:"), INPUT(_type='text', _id ="bottomRightLong", _name ="bottomRightLong", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"),_class="igsNumbers")
                     , _class="igsRow"),
+					DIV(_style="clear: both;"),
                     DIV(
                         DIV(DIV(INPUT(_id='circCB',  _name='rectCB', _type='checkbox'), _class='igsCB'), DIV(LABEL("Círculo:"), _class="igsOtherLabel")),
                         DIV(LABEL("Centro:"), _class='igsTabbedLabel'), 
                         DIV(LABEL("Latitud:"), INPUT(_type='text', _id ="centroLat", _name ="centroLat", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"), LABEL("Longitud:"), INPUT(_type='text', _id ="centroLong", _name ="centroLong", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"),_class="igsNumbers"), 
                         DIV(DIV(LABEL("Radio:"), _class="igsTabbedLabel"), DIV(INPUT(_type='text', _id ="radio", _name ="radio", _autocomplete="off", requires=IS_FLOAT_IN_RANGE(None,None), _disabled="disabled"), _class='igsNumbers'), _style="clear: both;")
                     , _class="igsRow"),
+                    DIV(_style="clear: both;"),
                     DIV(
                         DIV(INPUT(_type = "submit", _value= "Modelar"),_class = "igsSave")
                     )
@@ -90,6 +93,7 @@ def region():
 	De donde agarro esta info?? 
 	"""
 	ret = {}
+	ret['todas'] = "Todas"
 	
 	import gluon.contrib.simplejson
 	return gluon.contrib.simplejson.dumps({'rows': ret})
